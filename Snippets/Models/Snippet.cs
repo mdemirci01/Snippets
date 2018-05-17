6e8459eaf5fdf2571ade007016bb274c799e31e8
Mehmet <mehmetyilmaz0689@gmail.com>
2018-05-17 17:31:19 +0300

    Snipp Validations Added

--- Snippets/Models/Snippet.cs
+++ Snippets/Models/Snippet.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Snippets.Models
 {
     public class Snippet
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Snipp Adı")]
+        [StringLength(200)]
         public string Name { get; set; }
+        [Required]
+        [Display(Name = "Snipp Kodu")]
         public string Code { get; set; }
+        [Required]
+        [Display(Name = "Snipp Dili")]
+        [StringLength(200)]
         public string Language { get; set; }
     }
 }
